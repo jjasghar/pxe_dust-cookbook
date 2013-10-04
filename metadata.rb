ebeab0f11cd44b2c8baa375dd045b42141626250
Matt Ray <matthewhray@gmail.com>
2013-10-04 10:05:22 -0500

    depend on dnsmasq cookbook and newer apt cookbook for the library

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Configures local bootstrapping and installing operating systems via PXE booting."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.7.0"
+version          "2.0.0"
 depends          "apache2", ">= 1.6"
-depends          "tftp"
-recommends       "apt", ">= 1.3"
+depends          "dnsmasq", ">= 0.2"
+recommends       "apt", ">= 2.2"
 
 %w{ ubuntu debian }.each do |os|
   supports os
